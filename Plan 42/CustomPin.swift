d06a0abbc98c534921b64572753f937d3fb42687
kirillkomarovsky01 <komarovskycyril@yandex.ru>
2022-01-11 02:18:42 +0300

    42

---
+++ Plan 42/CustomPin.swift
@@ -0,0 +1,21 @@
+//
+//  CustomPin.swift
+//  Plan 42
+//
+//
+//
+
+import UIKit
+import MapKit
+
+class CustomPin: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(pinTitle: String, pinSubTitle: String, location: CLLocationCoordinate2D) {
+        self.title = pinTitle
+        self.subtitle = pinSubTitle
+        self.coordinate = location
+    }
+}
